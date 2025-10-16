6c6011c36218d10c932bb71380b81cec6976b7c1
Trent <vanderveen.615726@swl.k12.oh.us>
2025-10-16 08:19:37 -0400

    source

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        for (int nums1 = 0; nums1 <= 30; nums1++) {
+            System.out.print(nums1 + " ");
+        }
+
+        System.out.println();
+
+        for (int nums2 = 30; nums2 >= 0; nums2--) {
+            System.out.print(nums2 + " ");
+        }
+
+        for (int i = 0; i <= 18; i += 3) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        for (int i = 10; i >= 0; i -= 2) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+    }
+}
