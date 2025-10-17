8ba3595dfa04186fae69d309cd65f91bdccb0c24
Trent <vanderveen.615726@swl.k12.oh.us>
2025-10-17 08:16:59 -0400

    finshed task2

---
+++ src/Task2.java
@@ -0,0 +1,22 @@
+public class Task2 {
+    public static void main(String[] args) {
+
+        // Pattern 1
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        // Pattern 2
+        for (int i = 5; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
